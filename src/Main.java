5580b27a6c6319e7555bdfcf01f627bb6bca8f8f
lytlyt123456 <159670554+lytlyt123456@users.noreply.github.com>
2025-08-16 10:10:56 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入源代码文件路径: ");
+        String sourceFilePath = input.nextLine();
+        System.out.println("请输入保存中间代码的文件路径: ");
+        String desFilePath = input.nextLine();
+
         StringBuilder sourceCode = new StringBuilder();
-        Scanner fileReader = new Scanner(new File("src\\test_source_code.txt"));
+        Scanner fileReader = new Scanner(new File(sourceFilePath));
         while (fileReader.hasNext())
             sourceCode.append(fileReader.nextLine()).append('\n');
         fileReader.close();
@@ -26,7 +32,7 @@ public class Main {
         Parser parser = new Parser(tokens);
         List<IR> code = parser.getCode();
 
-        PrintWriter fileWriter = new PrintWriter(new File("src\\intermediate_representation.txt"));
+        PrintWriter fileWriter = new PrintWriter(new File(desFilePath));
         for(IR ir : code)
             fileWriter.println(ir.toString());
         fileWriter.println(code.size() + ":");
